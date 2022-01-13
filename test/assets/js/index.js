2e3e6681f4ad5d4c2a960c224907a31adde7590c
Nezer262 <86980660+Nezer262@users.noreply.github.com>
2022-01-13 20:56:43 +0300

    Add files via upload

---
+++ test/assets/js/index.js
@@ -0,0 +1,373 @@
+const HomeComponent = {
+render: () => {
+    return `
+    <div class="content" id="app">
+        <h1></h1>
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+}
+
+const individualsComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 1</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+} 
+
+const eliteComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+}
+
+const brandNewComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+} 
+
+const realComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+} 
+
+const departureComponent = {
+render: () => {
+    return `
+    <div class="content" id="app">
+        <h1></h1>
+        <div class="content-item">
+            <div class="item__status">
+                <a href="#" class="name">Алина</a>
+                <span class="online">Жду звонка</span>
+            </div>
+            <div class="item__descr">Пример описания текста</div>
+            <div class="item__details">
+                <div class="item__left">
+                    <div class="item-img">
+                        <a href="#">
+                            <img class="img-anketa" src="assets/img/anketa.jpg" alt="" width="150" height="190">
+                            <img class="anketaReal" src="assets/img/100.png" alt="">
+                        </a>
+                    </div>
+                    <div class="likes">
+                        <a class="like-item" href="#">0</a>
+                    </div> 
+                </div>
+                <div class="item__right">
+                    <div class="item__phone">8 (111) 111-11-11</div>
+                    <div class="item__place">Скрипт интим услуг</div>
+                    <div class="item__params">
+                        <p>Возраст: <span>18</span></p>
+                        <p class="parm">Вес: <span>52</span></p><br>
+                        <p>Рост: <span>175</span></p>
+                        <p class="parm">Грудь: <span>2</span></p>
+                    </div>
+                    <div class="item__price">
+                        <p><span>1 ЧАС:</span>1000 Р</p>
+                        <p><span>2 ЧАСА:</span>1000 Р</p>
+                        <p><span>НОЧЬ:</span>1000 Р</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+}
+
+const cheapComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+}
+
+const apartmentComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+} 
+
+const ErrorComponent = {
+render: () => {
+    return `
+    <section>
+        <h1>Error</h1>
+        <p>This is just a test</p>
+    </section>
+    `;
+}
+}
+
+const routes = [
+{ path: '/', component: HomeComponent, },
+{ path: '/individuals', component: individualsComponent, },
+{ path: '/elite', component: eliteComponent, },
+{ path: '/brandNew', component: brandNewComponent, },
+{ path: '/real', component:realComponent, },
+{ path: '/departure', component: departureComponent, },
+{ path: '/apartment', component: apartmentComponent, },
+{ path: '/cheap', component: cheapComponent, },
+];
+
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+
+const findComponentByPath = (path, routes) => routes.find(r => r.path.match(new RegExp(`^\\${path}$`, 'gm'))) || undefined;
+
+const router = () => {
+    // Find the component based on the current path
+    const path = parseLocation();
+    // If there's no matching route, get the "Error" component
+    const { component = ErrorComponent } = findComponentByPath(path, routes) || {};
+    // Render the component in the "app" placeholder
+    document.getElementById('app').innerHTML = component.render();
+};
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
